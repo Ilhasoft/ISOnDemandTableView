d0a7acbdc1f7f5c5c398acb855e603076e6d5501
Dielson Sales <dielsonscarvalho@gmail.com>
2016-12-21 21:47:24 -0300

    Initial Commit

---
+++ ISOnDemandTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ISOnDemandTableView
+//
+//  Created by Dielson Sales on 21/12/16.
+//  Copyright © 2016 Ilhasoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
