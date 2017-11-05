586e1018b805e820d4eb1f72617f2398bbf98511
Yves <raphael.bastos@me.com>
2017-11-05 15:52:38 -0300

    Corrections in project configs

---
+++ Example/SimpleTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  SimpleTableViewCell.swift
+//  ISOnDemandTableView
+//
+//  Created by Dielson Sales on 07/01/17.
+//  Copyright © 2017 Ilhasoft. All rights reserved.
+//
+
+import UIKit
+
+class SimpleTableViewCell: UITableViewCell, ISOnDemandTableViewCell {
+
+    @IBOutlet var lbText: UILabel!
+
+    func setupCell(with object: Any, at indexPath: IndexPath) {
+        guard let text = object as? String else { return }
+        lbText.text = text
+    }
+}
---
+++ Example/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ISOnDemandTableView
+//
+//  Created by Dielson Sales on 21/12/16.
+//  Copyright © 2016 Ilhasoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
